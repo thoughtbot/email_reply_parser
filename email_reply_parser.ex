f24b37c2e2c3929714a12cebc9a2f42f63785d68
Paul Smith <paulcsmith@users.noreply.github.com>
2017-04-24 15:40:18 -0400

    Add EmailReplyParser module

---
+++ email_reply_parser.ex
@@ -0,0 +1,24 @@
+defmodule EmailReplyParser do
+  def remove_original_email(email) do
+    email
+    |> remove_quoted_email
+    |> remove_trailing_newlines
+  end
+
+  defp remove_quoted_email(body) do
+    Enum.reduce(reply_header_formats(), body, fn(regex, email_body) ->
+      match = Regex.split(regex, email_body)
+      List.first(match)
+    end)
+  end
+
+  defp reply_header_formats do
+    [
+      ~r/\n\>?[[:space:]]*On.*<?\n?.*>?.*\n?wrote:\n?/,
+    ]
+  end
+
+  defp remove_trailing_newlines(body) do
+    Regex.replace(~r/\n+$/, body, "")
+  end
+end
